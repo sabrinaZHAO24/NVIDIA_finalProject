de4400f0af7ab9b6d3e1021cb27406bed89e1d47
sabrinaZHAO24 <109757256+sabrinaZHAO24@users.noreply.github.com>
2022-07-22 10:38:49 -0700

    Update finalProject.py

--- finalProject.py
+++ finalProject.py
@@ -17,11 +17,11 @@ Names = ['rafael nadal', 'roger federer', 'novak djokovic']
 
 #create testing image
 font = cv2.FONT_HERSHEY_SIMPLEX
-testImage = face_recognition.load_image_file('/home/sabrina/project/data/three2.jpg')
-positions = face_recognition.face_locations(testImage)
-allEncodings = face_recognition.face_encodings(testImage, positions)
+test = face_recognition.load_image_file('/home/sabrina/project/data/three2.jpg')
+positions = face_recognition.face_locations(test)
+allEncodings = face_recognition.face_encodings(test, positions)
 
-testImage = cv2.cvtColor(testImage, cv2.COLOR_RGB2BGR)
+test = cv2.cvtColor(testImage, cv2.COLOR_RGB2BGR)
 
 #face recognition for loop + showing the window using cv2
 for (top, right, bottom, left), face_encoding in zip(positions, allEncodings):
@@ -29,9 +29,9 @@ for (top, right, bottom, left), face_encoding in zip(positions, allEncodings):
     if True in matches:
         first_match_index = matches.index(True)
         name = Names[first_match_index]
-    cv2.rectangle(testImage, (left, top), (right, bottom), (252, 232, 131), 2)
-    cv2.putText(testImage, name, (left, top-6), font, .75, (252, 131, 202), 1)
-cv2.imshow("result", testImage)
+    cv2.rectangle(test, (left, top), (right, bottom), (252, 232, 131), 2)
+    cv2.putText(test, name, (left, top-6), font, .75, (252, 131, 202), 1)
+cv2.imshow("result", test)
 cv2.moveWindow('myWindow', 0, 0)
 if cv2.waitKey(0) == ord('q'):
     cv2.destroyAllWindows()
